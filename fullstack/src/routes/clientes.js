1e86b3bcf34c59e638b44d934e4f827e7ceded26
Daniele Paz <79173163+danielepaz404@users.noreply.github.com>
2025-09-04 07:56:10 +0000

    :sparkles: add endpoint de criacao de clientes

--- fullstack/src/routes/clientes.js
+++ fullstack/src/routes/clientes.js
@@ -1,23 +1,73 @@
-
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const { Cliente } = require('../db');
 
 /**
  * @openapi
  * /clientes:
- *  get:
- *    description: Retrieve a list of clients
- *    tags:
- *      - Clientes
- *    responses:
- *      200:
- *        description: Returns the list of clients in json format.
-*/
+ *   get:
+ *     description: Retorna a lista de clientes
+ *     tags:
+ *       - Clientes
+ *     responses:
+ *       200:
+ *         description: Retorna a lista de clientes em json
+ */
 router.get('/', async (req, res, next) => {
   const clientes = await Cliente.findAll();
   res.status(200).json(clientes);
 })
+
+/**
+ * @openapi
+ * /clientes:
+ *   post:
+ *     description: Cria um cliente
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *             required:
+ *               - nome
+ *               - email
+ *     tags:
+ *       - Clientes
+ *     responses:
+ *       400:
+ *         description: Paramétros inválidos
+ *       201:
+ *         description: Retorna o cliente que foi criado
+ */
+router.post('/', async (req, res, next) => {
+  try {
+    const { nome, email } = req.body;
+
+    if (!nome || !email) {
+      return res.status(400).json({ error: 'Nome e email são obrigatórios' });
+    }
+    const newCliente = await Cliente.build({ nome, email });
+
+    await newCliente.validate()
+
+    await newCliente.save();
+
+    res.status(201).json(newCliente);
+  } catch (error) {
+
+    if (error?.errors) {
+      return res.status(400).json({ error: error.errors.map(e => e.message).join(', ') });
+    }
+
+    next(error);
+  }
+});
 
 
 module.exports = router;
